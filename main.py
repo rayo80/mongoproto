ec9e1d1fa66cf4cae9e174dcf34d195bcdac61bb
Sergio <sergiovilla242@gmail.com>
2022-09-21 11:55:33 -0500

    compiler readme added

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ print(position_44)
 valor=pst.singleprotodatatest(position_44)
 #valor = pst.singleprototest()
 print(valor)
-#sv.enviar_proto(valor)
+sv.enviar_proto(valor)
 
--- services.py
+++ services.py
@@ -3,13 +3,12 @@ import os
 import requests
 
 # BETA LOCAL
-parametros = {"token": "123456"}
 api = "http:zenbus.net/provider/post.proto?apiKey=test"
 
 
 def enviar_proto(valor):
     url = api
-    r = requests.post(url, headers={'Content-Type': 'application/protobuf'}, data=valor)
+    r = requests.post(url, headers={'Content-Type': 'application/x-protobuf'}, data=valor)
     r.raise_for_status()
     return r
 
